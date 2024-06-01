96031f7b2bc744c122b3468b389ab02bdfc27e6f
rudresh <srudresh288@gmail.com>
2024-06-01 19:50:03 +0530

    nav-icon-on-mobile

---
+++ script.js
@@ -0,0 +1,11 @@
+function showsidebar()
+{
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex" 
+}
+
+function hidesidebar()
+{
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex" 
+}
